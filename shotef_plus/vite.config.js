2805d1ce7df6da4d910b703824e2774d6b104005
AssafBenIshay <b.i.app.solutions@gmail.com>
2024-12-20 17:10:06 +0200

    additing vite.config.js

--- shotef_plus/vite.config.js
+++ shotef_plus/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import InjectCSS from '@itsy/vite-css-inject';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    InjectCSS()
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@tests": path.resolve(__dirname, "./tests")
+    }
+  }
 })
