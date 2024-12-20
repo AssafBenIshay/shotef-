8ac060c4f837c3d87b842d85fca09f93c9381ec2
AssafBenIshay <b.i.app.solutions@gmail.com>
2024-12-20 17:24:45 +0200

    reverted vite config

--- shotef_plus/vite.config.js
+++ shotef_plus/vite.config.js
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import InjectCSS from '@itsy/vite-css-inject';
-import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    InjectCSS()
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@tests": path.resolve(__dirname, "./tests")
-    }
-  }
+  plugins: [react()],
 })
